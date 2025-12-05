e366d52d0a7490dc4c6242246611455dc03405e8
yash vadachhak <vadchhakyashh@gmail.com>
2025-12-05 10:56:01 +0530

    add the toggal functionality and other

--- src/App.jsx
+++ src/App.jsx
@@ -4,48 +4,59 @@ import ChatForm from "./components/ChatForm";
 import ChatMessage from "./components/ChatMessage";
 
 const App = () => {
-  const [chatHistory,setChatHistory] = useState([]);
+  const [chatHistory, setChatHistory] = useState([]);
+  const [showChatbot,setShowChatbot] = useState(false);
   const chatBodyRef = useRef();
 
-  const generateBotResponse = async(history) =>{
+  const generateBotResponse = async (history) => {
     //replacing the last bot's "Thinking" text with the response(helper function to update chat history)
-    const updateHistory = (text) =>{
-      setChatHistory(prev => [...prev.filter(msg => msg.text !== "Thinking..."),{role:"model",text}])
-    }
+    const updateHistory = (text) => {
+      setChatHistory((prev) => [
+        ...prev.filter((msg) => msg.text !== "Thinking..."),
+        { role: "model", text },
+      ]);
+    };
 
-    history = history.map(({role,text}) => ({role,parts:[{text}]}));
+    history = history.map(({ role, text }) => ({ role, parts: [{ text }] }));
 
-     const requestOption = {
+    const requestOption = {
       method: "POST",
-      headers: {"Content-Type" : "application/json"},
-      body : JSON.stringify({contents: history })
-     }
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify({ contents: history }),
+    };
 
-     try {
+    try {
       // Make the api call to get the bot's response
-       const response = await fetch(import.meta.env.VITE_API_URL, requestOption);       
-       
-       const data = await response.json();
-       if(!response.ok) throw new Error(data.error.message || "Something went wrong!")
+      const response = await fetch(import.meta.env.VITE_API_URL, requestOption);
 
-        // Clean and update chat history with bot's response
-       const apiResponseText = data.candidates[0].content.parts[0].text.replace(/\*\*(.*?)\*\*/g,"$1").trim();
-       updateHistory(apiResponseText);
-        
-     } catch (error) {
+      const data = await response.json();
+      if (!response.ok)
+        throw new Error(data.error.message || "Something went wrong!");
+
+      // Clean and update chat history with bot's response
+      const apiResponseText = data.candidates[0].content.parts[0].text
+        .replace(/\*\*(.*?)\*\*/g, "$1")
+        .trim();
+      updateHistory(apiResponseText);
+    } catch (error) {
       console.log(error);
-      
-     }
-     
-  }
+    }
+  };
 
-  useEffect(()=>{
+  useEffect(() => {
     //Auto-scroll whenever chat history updates
-    chatBodyRef.current.scrollTo({top: chatBodyRef.current.scrollHeight,behavior:"smooth"})
-  },[chatHistory])
-  
+    chatBodyRef.current.scrollTo({
+      top: chatBodyRef.current.scrollHeight,
+      behavior: "smooth",
+    });
+  }, [chatHistory]);
+
   return (
-    <div className="container">
+    <div className={`container ${showChatbot ? "show-chatbot" : ""}`}>
+      <button onClick={()=>setShowChatbot((prev) => !prev) } id="chatbot-toggler">
+        <span className="material-symbols-rounded">mode_comment</span>
+        <span className="material-symbols-rounded">close</span>
+      </button>
       <div className="chatbot-popup">
         {/* Chatbot Header */}
         <div className="chat-header">
@@ -53,7 +64,7 @@ const App = () => {
             <ChatbotIcon />
             <h2 className="logo-text">Chatbot</h2>
           </div>
-          <button className="material-symbols-rounded">
+          <button onClick={()=>setShowChatbot((prev) => !prev) } className="material-symbols-rounded">
             keyboard_arrow_down
           </button>
         </div>
@@ -65,16 +76,19 @@ const App = () => {
               Hey there 👋 <br /> How can I help you today?
             </p>
           </div>
-           
-           {/* render the chat hstory dynamically */}
-          {chatHistory.map((chat,index)=>(
-              <ChatMessage key={index} chat={chat}/>      
+
+          {/* render the chat hstory dynamically */}
+          {chatHistory.map((chat, index) => (
+            <ChatMessage key={index} chat={chat} />
           ))}
-          
         </div>
         {/* Chatbot Footer */}
         <div className="chat-footer">
-          <ChatForm chatHistory={chatHistory} setChatHistory={setChatHistory} generateBotResponse={generateBotResponse}/>
+          <ChatForm
+            chatHistory={chatHistory}
+            setChatHistory={setChatHistory}
+            generateBotResponse={generateBotResponse}
+          />
         </div>
       </div>
     </div>
